21e345804a2237f84a46be37d7edf138bde79030
jsochoaco <sebastianochoa05@gmail.com>
2023-08-12 19:38:37 -0500

    Modelo Dog 1208

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -2,11 +2,41 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('dog', {
-    name: {
+  // Definimos 
+  const Dog = sequelize.define('dog', {
+    id: { // Id
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      primaryKey: true,
+      autoIncrement: true,
+      initialAutoIncrement: 265, // Los que no tienen id (agregados), se creen desde el 265 en adelante
+    },
+    image: {
+      type: DataTypes.STRING, // Image en la api es un objeto, se debe extraer la info de la URL 
+      allowNull: true, // Cuando se crean perros nuevos, no se adjunta imagenes, por eso no debe ser obligatoria en la DB. 
+    },
+    name: { // Nombre
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: { // Altura 
+      type: DataTypes.STRING, // Altura en la api es un objeto con los datos para sistema imperial y metrico, se debe extraer
+      allowNull: false,
+    },
+    weight: { // Peso
+      type: DataTypes.STRING, // Peso en la api es un objeto con los datos para sistema imperial y metrico, se debe extraer
+      allowNull: false,
+    },
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   });
+
+  return Dog
 };
+
+// Para no confundir los ID's, los perros que se creen serán con un ID posterior a el maximo que trae la API (264). Por ende, todo perro nuevo que se cree, iniciará con el ID 265 en adelante. 
+// Es posible usar BeforeCreate o el autoincremente con un inicio en 265
+
+
