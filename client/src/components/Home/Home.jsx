8245b94ab83300fee50a895bb68f8335a6b39c9d
jsochoaco <sebastianochoa05@gmail.com>
2023-08-29 11:05:19 -0500

    2908 FrontEnd V1

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux"
 import style from "./Home.module.css"
 import Filtros from "./Filtros"
 import Ordenador from "./Ordenador"
+import { useState } from "react"
 
 
 const Home = (props) => {
@@ -14,6 +15,17 @@ const Home = (props) => {
     const clearFilters = () => {
         dispatch(actions.clearFilter())
       };
+    // Paginado (
+    const [pagina, setPagina] = useState(1)
+    const porPagina = 8
+    const ultimoElemento = pagina*porPagina
+    const primerElemento = ultimoElemento - porPagina
+    const actualDogs = allDogs.slice(primerElemento, ultimoElemento)
+    const totalPages = Math.ceil(allDogs.length / porPagina);
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i);
+    }
     // Rendereizado
     return (
         <div className={style.general}>
@@ -23,9 +35,23 @@ const Home = (props) => {
             </div>
             <div className={style.divboton}>
                 <button className={style.botonclean} onClick={clearFilters}> Clear filters and orders </button>
-                <button className={style.botones} onClick={data}> Refresh data </button>
+                {/* <button className={style.botones} onClick={data}> Refresh data </button> */}
             </div>
-            <CardDogs dogs = {allDogs} intermedia = {intermedia} temperamentos = {temperamentos} />
+            <div className={style.paginado}>
+                <button className= {style.botonpag} onClick={() => setPagina(pagina - 1)} disabled={pagina === 1}> Previous</button>
+                {pageNumbers.map((pageNumber) => (
+                    <button key={pageNumber} className={pageNumber === pagina ? style.pagina : style.paginaboton} onClick={() => setPagina(pageNumber)}>{pageNumber}</button>
+                ))}
+                <button className= {style.botonpag} onClick={() => setPagina(pagina + 1)} disabled={ultimoElemento >= allDogs.length}>Next</button>
+            </div>
+            <CardDogs dogs = {actualDogs} intermedia = {intermedia} temperamentos = {temperamentos} />
+            <div className={style.paginado2}>
+                <button className= {style.botonpag} onClick={() => setPagina(pagina - 1)} disabled={pagina === 1}> Previous</button>
+                {pageNumbers.map((pageNumber) => (
+                    <button key={pageNumber} className={pageNumber === pagina ? style.pagina : style.paginaboton} onClick={() => setPagina(pageNumber)}>{pageNumber}</button>
+                ))}
+                <button className= {style.botonpag} onClick={() => setPagina(pagina + 1)} disabled={ultimoElemento >= allDogs.length}>Next</button>
+            </div>
         </div>
     )
 }; export default Home
